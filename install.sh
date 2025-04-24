0e0c63617a8827ca1428a9e71145c63823f90928
nxppru <nxppru@qq.com>
2025-04-24 21:39:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ case "$DISTRIB_RELEASE" in
 esac
 
 # feed url
-repository_url="https://nikkinikki.pages.dev"
+repository_url="https://github.com/nxppru/OpenWrt-nikki/tree/gh-pages/SNAPSHOT/"
 feed_url="$repository_url/$branch/$arch/nikki"
 
 if [ -x "/bin/opkg" ]; then
